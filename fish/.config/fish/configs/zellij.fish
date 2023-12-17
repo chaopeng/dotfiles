8808dcf7dd62cea353d921021860d9a1b9148a0b
chaopeng <chaopeng@chaopeng.me>
2023-12-17 15:38:14 -0500

    [zellij] fish source completion/
    do not why this issue. just workaround.

---
+++ fish/.config/fish/configs/zellij.fish
@@ -0,0 +1,6 @@
+# fish in zellij have issue to load commands completions from ~/.config/fish/completions.
+if [ $IS_ZELLIJ == 1 ]
+    for f in (ls $HOME/.config/fish/completions)
+        source $HOME/.config/fish/completions/$f
+    end
+end
