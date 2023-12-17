2ba2c0b98695382423ea2585bb35df0cbba18ad9
chaopeng <chaopeng@chaopeng.me>
2023-12-17 15:38:14 -0500

    [fish] source user completions dir manully

--- fish/.config/fish/configs/fix-completions.fish
+++ fish/.config/fish/configs/fix-completions.fish
@@ -1,13 +1,5 @@
-# fish in zellij have issue to load commands completions from ~/.config/fish/completions.
-if [ $IS_ZELLIJ = 1 ]
-    for f in (ls $HOME/.config/fish/completions)
-        source $HOME/.config/fish/completions/$f
-    end
+# fish in zellij / vscode have issue to load commands completions from ~/.config/fish/completions.
+for f in (ls $HOME/.config/fish/completions)
+    source $HOME/.config/fish/completions/$f
 end
 
-# fish in vscode have issue to load commands completions from ~/.config/fish/completions.
-if [ $IS_VSCODE = 1 ]
-    for f in (ls $HOME/.config/fish/completions)
-        source $HOME/.config/fish/completions/$f
-    end
-end
