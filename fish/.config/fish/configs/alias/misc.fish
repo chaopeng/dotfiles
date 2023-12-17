1f89e262a82a6041d92e83e41c334c46e6d81b07
chaopeng <chaopeng@chaopeng.me>
2023-12-17 15:38:08 -0500

    [kitty] change alias "c" for clear in kitty

--- fish/.config/fish/configs/alias/misc.fish
+++ fish/.config/fish/configs/alias/misc.fish
@@ -1,7 +1,10 @@
 # misc alias
 
+# c is short for clear and clear scroll buffer.
+# From https://github.com/kovidgoyal/kitty/issues/268
+alias c "printf '\033[2J\033[3J\033[1;1H'"
+
 alias m micro
-alias c clear
 alias v nvim
 alias vi nvim
 alias vim nvim
