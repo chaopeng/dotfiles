c5634d3d7f4d7f4d85801dd1b8dc50c89de2581c
chaopeng <chaopeng@chaopeng.me>
2023-12-17 16:16:38 -0500

    [fish] set xdg_config path

--- fish/.config/fish/configs/main.fish
+++ fish/.config/fish/configs/main.fish
@@ -3,6 +3,7 @@ set -xg LC_TIME zh_CN.UTF-8
 set -xg LC_ALL en_US.UTF-8
 
 set -xg FISH_CFG_PATH $HOME/.config/fish/configs
+set -xg XDG_CONFIG_HOME $HOME/.config
 
 source $FISH_CFG_PATH/path.fish
 source $FISH_CFG_PATH/choose-editor.fish
