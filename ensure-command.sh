7d4b085868fd9c5e704c3ea5388777963c5cce7f
chaopeng <chaopeng@chaopeng.me>
2023-12-26 01:04:14 -0500

    [Makefile] chmod +x ensure-command.sh

--- ensure-command.sh
+++ ensure-command.sh
@@ -2,7 +2,7 @@
 
 # Ensure the command exists
 
-if ! command -v "$command" >/dev/null 2>&1; then
-  echo "Error: '$command' is not installed. Please install it and try again." >&2
+if ! command -v "$1" >/dev/null 2>&1; then
+  echo "Error: '$1' is not installed. Please install it and try again." >&2
   exit 1
 fi
