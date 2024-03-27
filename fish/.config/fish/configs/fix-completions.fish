ec33e78982da6f646718c017c4521ff79f250349
chaopeng <chaopeng@chaopeng.me>
2024-03-27 00:20:04 -0400

    [fish] use builtin ls in script

--- fish/.config/fish/configs/fix-completions.fish
+++ fish/.config/fish/configs/fix-completions.fish
@@ -1,5 +1,4 @@
 # fish in zellij / vscode have issue to load commands completions from ~/.config/fish/completions.
-for f in (ls $HOME/.config/fish/completions)
+for f in (/bin/ls $HOME/.config/fish/completions)
     source $HOME/.config/fish/completions/$f
 end
-
