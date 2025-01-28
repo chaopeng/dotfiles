4e66aa5d31842f66d369840dfc4e46f997a4e3b4
chaopeng <chaopeng@chaopeng.me>
2025-01-27 21:46:42 -0500

    [precheck] remove gitconfig check

--- prestow-check.sh
+++ prestow-check.sh
@@ -12,20 +12,19 @@
 # - ~/bin/browsers exists on Mac
 # - ~/Library/Application Support/Code/User exists on Mac
 # - ~/.bashrc is backup and removed
-# - ~/.gitconfig is backup and removed
 ############################################################
 
 function mkdir_if_not_exists() {
-	if [ ! -d "$1" ]; then
-		mkdir $1
-	fi
+  if [ ! -d "$1" ]; then
+    mkdir $1
+  fi
 }
 
 function fail_if_file_exists_but_a_link() {
-	if [ -e "$1" ] && [ ! -L "$1" ]; then
-		echo "$1 exists, please backup and remove"
-		exit 1
-	fi
+  if [ -e "$1" ] && [ ! -L "$1" ]; then
+    echo "$1 exists, please backup and remove"
+    exit 1
+  fi
 }
 
 mkdir_if_not_exists ~/.config/fish
@@ -35,9 +34,8 @@ mkdir_if_not_exists ~/.config/lazygit
 mkdir_if_not_exists ~/bin
 
 if [[ "$(uname -s)" == "Darwin" ]]; then
-	mkdir_if_not_exists ~/bin/browsers
-	mkdir_if_not_exists ~/Library/Application\ Support/Code/User/
+  mkdir_if_not_exists ~/bin/browsers
+  mkdir_if_not_exists ~/Library/Application\ Support/Code/User/
 fi
 
 fail_if_file_exists_but_a_link ~/.bashrc
-fail_if_file_exists_but_a_link ~/.gitconfig
