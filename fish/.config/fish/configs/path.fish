7ba4e948fe130d055d62252b474c6fbcf36a75e9
chaopeng <chaopeng@chaopeng.me>
2025-03-19 11:57:57 -0400

    [fish] add code to PATH

--- fish/.config/fish/configs/path.fish
+++ fish/.config/fish/configs/path.fish
@@ -3,5 +3,9 @@ set -xg PATH $HOME/bin $HOME/go/bin $HOME/.cargo/bin $PATH
 if [ $IS_MAC = 1 ]
     set -xg PATH $HOME/bin/browsers $PATH
     eval (/opt/homebrew/bin/brew shellenv)
+
+    if not which code
+        set -xg PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" $PATH
+    end
 end
 
